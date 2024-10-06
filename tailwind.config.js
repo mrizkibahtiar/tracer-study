fce88376f85d1296c3e116f4349555514788a13e
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-06 21:32:49 +0700

    inisialisasi projek

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./client/**/*.{html,js}", "./client/styles**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
