68ba9f51086f7d1b1ca1ccabe87bb3f28e0d0866
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-10 14:43:29 +0700

    Update admin.js

--- router/admin.js
+++ router/admin.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
+const { isAdmin } = require('../middleware/auth');
 
-router.get('/admin', adminController.index);
-router.get('/admin/alumni-form', adminController.alumniForm);
+
+router.get('/admin', isAdmin, adminController.index);
+router.get('/admin/alumni-form', isAdmin, adminController.alumniForm);
 
 
 module.exports = router;
