679adf6039eed47d8e0a9ae0d7feef68fca712f7
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-10 14:43:18 +0700

    Update alumni.js

--- controllers/alumni.js
+++ controllers/alumni.js
@@ -7,9 +7,12 @@ module.exports = {
         if (!req.session.user) {
             return res.redirect('/loginPage'); // Redirect ke halaman login jika user belum login
         } else {
-            const { nisn, role } = req.session.user;
+            const { nisn, role } = req.session.user._doc;
             const alumni = await Alumni.findOne({ nisn: nisn });
             return res.render('pages/alumni/dashboard', { alumni: alumni });
         }
+    },
+    alumniForm: function (req, res) {
+        res.render('pages/alumni/alumni_form');
     }
 }
