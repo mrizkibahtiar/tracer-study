55f961fcde1e04bf67ff56e43ec341b84c82330c
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-10 13:44:10 +0700

    menambahkan router admin/alumni-form

--- router/admin.js
+++ router/admin.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 router.get('/admin', adminController.index);
+router.get('/admin/alumni-form', adminController.alumniForm);
+
 
 module.exports = router;
