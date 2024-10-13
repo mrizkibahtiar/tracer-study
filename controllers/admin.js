7a47e78c6178c1a1b75d38bfa28025bbe91816c2
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-13 21:00:42 +0700

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Admin = require('../models/admin');
+const Alumni = require('../models/alumni');
 const mongoose = require('mongoose');
 
 
@@ -16,5 +17,34 @@ module.exports = {
 
     alumniForm: function (req, res) {
         res.render('pages/admin/alumni_form');
+    },
+
+    store: async function (req, res) {
+        const { name, nisn, password, email, graduationYear } = req.body;
+        try {
+            if (email && password) {
+                const alumni = await Alumni.create({
+                    nisn: nisn,
+                    name: name,
+                    password: password,
+                    email: email,
+                    graduationYear: graduationYear
+                });
+                res.redirect('/admin/alumni-form');
+                return alumni
+            } else {
+                const alumni = Alumni.create({
+                    nisn: nisn,
+                    name: name,
+                    password: password,
+                });
+                res.redirect('/admin/alumni-form');
+                return alumni
+            }
+        } catch (err) {
+            console.log(err);
+            res.redirect('/admin/alumni-form');
+        }
+        res.redirect('/admin');
     }
 }
