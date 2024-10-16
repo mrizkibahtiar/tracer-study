98b6232f89178fdae2a4004b873d15d3f6ff22a0
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-16 21:13:25 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,17 @@
 module.exports = {
+    checkLogin: (req, res, next) => {
+        // Cek apakah user sudah login
+        if (req.session && req.session.user) {
+            if (req.session.user.role === 'admin') {
+                return res.redirect('/admin');
+            }
+            if (req.session.user.role === 'alumni') {
+                return res.redirect('/alumni');
+            }
+        }
+        // Jika belum login, lanjutkan ke halaman login
+        next();
+    },
     isAdmin: (req, res, next) => {
         if (req.session.user && req.session.user.role === 'admin') {
             return next(); // Akses diizinkan, lanjutkan ke rute berikutnya
