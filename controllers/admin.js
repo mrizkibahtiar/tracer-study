724ea7a658588ce2c215744a084d22d57553de7f
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-20 17:35:58 +0700

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,6 @@ module.exports = {
         } else {
             const { email, role } = req.session.user._doc;
             const admin = await Admin.findOne({ email: email });
-            console.log(admin);
             return res.render('pages/admin/dashboard', { admin: admin });
         }
     },
@@ -21,7 +20,6 @@ module.exports = {
 
     store: async function (req, res) {
         const { name, nisn, password, email, graduationYear } = req.body;
-        console.log(req.body);
         try {
             // Membuat objek untuk alumni
             const alumniData = {
@@ -48,7 +46,6 @@ module.exports = {
 
     viewAlumniList: async function (req, res) {
         const alumni = await Alumni.find({});
-        console.log(alumni);
         return res.render('pages/admin/alumni_list', { alumni: alumni });
     },
 
@@ -64,8 +61,27 @@ module.exports = {
             const alumni = await Alumni.findOneAndDelete({ nisn: nisn });
             return res.redirect('/admin/alumni-list');
         } catch (err) {
-            console.log(err);
             return res.redirect('/admin/alumni-list/' + nisn);
+        }
+    },
+
+    alumniEdit: async function (req, res) {
+        const { nisn } = req.params;
+        const alumni = await Alumni.findOne({ nisn: nisn });
+        console.log(alumni);
+        return res.render('pages/admin/alumni_edit', { alumni: alumni });
+    },
+
+    alumniUpdate: async function (req, res) {
+        const { nisn } = req.params;
+        const { name, password, email, graduationYear } = req.body;
+        try {
+            const alumni = await Alumni.findOneAndUpdate({ nisn: nisn }, { name: name.trim(), password: password.trim(), email: email.trim(), graduationYear: graduationYear.trim() }, { new: true });
+            req.flash('success_msg', 'Alumni berhasil diperbarui!');
+            return res.redirect('/admin/alumni-edit/' + nisn);
+        } catch (err) {
+            req.flash('error_msg', 'Terjadi kesalahan. Mohon coba lagi.');
+            return res.redirect('pages/admin/alumni-edit/' + nisn);
         }
     }
 }
