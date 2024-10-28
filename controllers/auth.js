53a7f5603dac067ff8d38d6fb4af7de8d8908dce
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-10-28 08:46:50 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,42 @@
 const Alumni = require('../models/alumni');
 const Admin = require('../models/admin');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
+
     login: async (req, res) => {
         const { nisn, password } = req.body;
+        // Cari alumni berdasarkan NISN atau email
         const alumni = await Alumni.findOne({
             $or: [
                 { nisn: nisn.trim() },
                 { email: nisn.trim() }
             ]
         });
+        // Cari admin berdasarkan email
         const admin = await Admin.findOne({ email: nisn.trim() });
         const user = alumni || admin;
+
+        // Jika user tidak ditemukan
         if (!user) {
             return res.render('pages/login', { error: 'User not found' });
         }
-        if (user.password !== password.trim()) {
+
+        // Bandingkan password yang dimasukkan dengan password yang di-hash di database
+        const isPasswordValid = await bcrypt.compare(password.trim(), user.password);
+        if (!isPasswordValid) {
             return res.render('pages/login', { error: 'Password incorrect' });
         }
+
+        // Jika user adalah alumni
         if (alumni) {
-            req.session.user = { ...alumni, role: 'alumni' };
+            req.session.user = { ...alumni.toObject(), role: 'alumni' };
             req.session.save();
             return res.redirect('/alumni');
-        } else {
-            req.session.user = { ...admin, role: 'admin' };
+        } else {  // Jika user adalah admin
+            req.session.user = { ...admin.toObject(), role: 'admin' };
             req.session.save();
             return res.redirect('/admin');
         }
