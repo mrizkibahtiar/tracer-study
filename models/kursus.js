04b2a7f9cf8c92a3184802b22fe81eee4ca1530a
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-09 15:54:44 +0700

    Create kursus.js

---
+++ models/kursus.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const kursusSchema = new mongoose.Schema({
+    alumniId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Alumni'
+    },
+    namaKursus: {
+        type: String,
+        required: true
+    },
+    alamatKursus: {
+        type: String,
+        required: true
+    },
+    bidangKursus: {
+        type: String,
+        required: true
+    },
+    tanggalMulai: {
+        type: Date,
+        required: true
+    },
+    tanggalSelesai: {
+        type: Date,
+        required: true
+    },
+});
+
+const Kursus = mongoose.model('Kursus', kursusSchema);
