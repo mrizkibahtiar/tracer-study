713d40f7a09e14b1abce64dea728dbcc6d599f06
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-11 08:08:53 +0700

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/auth');
 const { isAdmin, isAlumni } = require('../middleware/auth');
 
 
-router.post('/login', authController.login);
+router.post('/loginPage', authController.login);
 router.get('/logout', authController.logout);
 
 module.exports = router
