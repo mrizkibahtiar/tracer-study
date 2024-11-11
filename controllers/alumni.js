f121f66e0ddf32ba80bca0351f68ac1d2dde103e
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-11 08:08:41 +0700

    Update alumni.js

--- controllers/alumni.js
+++ controllers/alumni.js
@@ -9,16 +9,33 @@ module.exports = {
         } else {
             const { nisn } = req.session.user;
             const alumni = await Alumni.findOne({ nisn: nisn });
-            return res.render('pages/alumni/alumni_form', { alumni: alumni })
+            return res.render('pages/alumni/dashboard', { alumni: alumni })
         }
     },
     profile: async function (req, res) {
         if (!req.session.user) {
-
+            return res.redirect('/loginPage')
         } else {
             const { nisn } = req.session.user;
             const alumni = await Alumni.findOne({ nisn: nisn });
             return res.render('pages/alumni/profile', { alumni: alumni })
         }
+    },
+    showForm: async function (req, res) {
+        if (!req.session.user) {
+            return res.redirect('/loginPage');
+        } else {
+            const { nisn } = req.session.user;
+            const alumni = await Alumni.findOne({ nisn: nisn });
+            return res.render('pages/alumni/alumni_form', { alumni: alumni });
+        }
+    },
+
+    saveForm1: async function (req, res) {
+        const { nisn } = req.params;
+        const email = req.body.email.trim();
+        const tahunLulus = req.body.tahunLulus.trim();
+        const kegiatan = req.body.kegiatan.trim();
+        console.log(nisn, email, tahunLulus, kegiatan);
     }
 }
