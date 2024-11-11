e43014be60b006e37431d7f41cf9052a5c688eaf
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-11 08:08:51 +0700

    Update alumni.js

--- router/alumni.js
+++ router/alumni.js
@@ -9,5 +9,7 @@ const { isAlumni } = require('../middleware/auth');
 // router.put('alumni/user/:id', userController.update);
 router.get('/alumni', isAlumni, userController.index);
 router.get('/alumni/profile', isAlumni, userController.profile)
+router.get('/alumni/alumni-form', isAlumni, userController.showForm);
+router.post('/alumni/alumni-form/:nisn', isAlumni, userController.saveForm1);
 
 module.exports = router
