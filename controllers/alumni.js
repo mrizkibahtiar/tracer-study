0dee110c1b3e594a2fbfafb349f8c745b0f775bd
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-13 11:20:44 +0700

    Update alumni.js

--- controllers/alumni.js
+++ controllers/alumni.js
@@ -1,5 +1,6 @@
 
 const Alumni = require('../models/alumni');
+const TracerStudy = require('../models/tracerStudy');
 const mongoose = require('mongoose');
 
 module.exports = {
@@ -27,15 +28,37 @@ module.exports = {
         } else {
             const { nisn } = req.session.user;
             const alumni = await Alumni.findOne({ nisn: nisn });
+            console.log(alumni)
             return res.render('pages/alumni/alumni_form', { alumni: alumni });
         }
     },
 
     saveForm1: async function (req, res) {
-        const { nisn } = req.params;
+        const { nisn } = req.session.user;
         const email = req.body.email.trim();
         const tahunLulus = req.body.tahunLulus.trim();
         const kegiatan = req.body.kegiatan.trim();
-        console.log(nisn, email, tahunLulus, kegiatan);
+        try {
+            const alumni = await Alumni.findOne({ nisn: nisn });
+            const newTracerStudy = {
+                alumniId: alumni._id,
+                email: email,
+                tahunLulus: tahunLulus,
+                kegiatan: kegiatan
+            };
+            await TracerStudy.create(newTracerStudy);
+            const kegiatanUrl = kegiatan.toLowerCase().replace(/\s+/g, '-');
+            return res.redirect(`/alumni/alumni-form2-${kegiatanUrl}/${nisn}`);
+        } catch (err) {
+            console.error(err);
+            return res.render('pages/alumni/alumni_form', { error: 'Terjadi kesalahan. Mohon coba lagi.' });
+        }
+    },
+
+    showForm2Kursus: async function (req, res) {
+        const nisn = req.params.nisn;
+        const alumni = await Alumni.findOne({ nisn: nisn });
+        const form1 = await TracerStudy.findOne({ alumniId: alumni._id });
+        return res.render('pages/alumni/kursus', { form1: form1 });
     }
 }
