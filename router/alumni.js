d343c09d5303335cd2c81e45072b12e983c9a302
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-11-18 08:12:56 +0700

    Update alumni.js

--- router/alumni.js
+++ router/alumni.js
@@ -9,14 +9,7 @@ router.get('/alumni', isAlumni, userController.index);
 router.get('/alumni/profile', isAlumni, userController.profile)
 
 router.get('/alumni/alumni-form', isAlumni, userController.showForm)
-router.post('/alumni/alumni-form/:nisn', isAlumni, userController.saveForm1)
+router.post('/alumni/alumni-form/:nisn', isAlumni, userController.saveForm)
 
-router.route('/alumni/alumni-form2-kursus/:nisn')
-    .get(isAlumni, userController.showForm2Kursus)
-// .post(isAlumni, userController.saveForm2Kursus)
-
-
-// router.get('/alumni/alumni-form3/:nisn', isAlumni, userController.showForm3)
-// router.post('/alumni/alumni-form3/:nisn', isAlumni, userController.saveForm3)
 
 module.exports = router
