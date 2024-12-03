d58f1a2b92dca8d47cf18bbb0d65f529afcb52f0
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-12-03 19:25:12 +0700

    Update admin.js

--- router/admin.js
+++ router/admin.js
@@ -15,4 +15,6 @@ router.delete('/admin/alumni-list/:nisn', isAdmin, adminController.deleteAlumni)
 router.put('/admin/alumni-edit/:nisn', isAdmin, adminController.alumniUpdate);
 router.put('/admin/alumni-edit/password/:nisn', isAdmin, adminController.alumniUpdatePassword);
 router.get('/admin/profile', isAdmin, adminController.profile);
+router.put('/admin/profile-edit/:adminId', isAdmin, adminController.alumniUpdate);
+router.put('/admin/profile-edit/password/:adminId', isAdmin, adminController.alumniUpdatePassword);
 module.exports = router;
